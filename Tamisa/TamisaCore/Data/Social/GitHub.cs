3286065c6369028424c7d40d5ea8a751a9fb7908
Filipe Oliveira <contato@fmoliveira.com.br>
2013-07-24 18:52:08 -0300

    Beginning of the Tamisa Project.

---
+++ Tamisa/TamisaCore/Data/Social/GitHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamisa.Data.Social
+{
+    // DescriptionAttribute with social network name
+    public class GitHub : ISocial
+    {
+        public string ProfileID { get; set; }
+    }
+}
---
+++ Tamisa/TamisaCore/Data/UserProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamisa.Data.Social;
+
+namespace Tamisa.Data
+{
+    public class UserProfile
+    {
+        #region Properties
+
+        public string FullName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string Company { get; set; }
+
+        public string Location { get; set; }
+
+        public string Biography { get; set; }
+
+        public List<ISocial> Social { get; set; }
+
+        #endregion
+    }
+}
