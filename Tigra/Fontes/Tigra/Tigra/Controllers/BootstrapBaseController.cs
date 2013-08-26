81991ed08a28dc6e2de93ad2e2f3cb7037804fb3
Filipe Oliveira <contato@fmoliveira.com.br>
2013-08-26 20:08:32 -0300

    Creating Tigra project with package Twitter.Bootstrap.MVC4 and updating Bootstrap to 3.0.0.

---
+++ Tigra/Fontes/Tigra/Tigra/Controllers/BootstrapBaseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using BootstrapSupport;
+
+namespace BootstrapMvcSample.Controllers
+{
+    public class BootstrapBaseController: Controller
+    {
+        public void Warning(string message)
+        {
+            TempData.Add(Alerts.WARNING, message);
+        }
+
+        public void Success(string message)
+        {
+            TempData.Add(Alerts.SUCCESS, message);
+        }
+
+        public void Information(string message)
+        {
+            TempData.Add(Alerts.INFORMATION, message);
+        }
+
+        public void Error(string message)
+        {
+            TempData.Add(Alerts.DANGER, message);
+        }
+    }
+}
