581e90f6a0165e7e4829d4951813a5345341c75e
Filipe Oliveira <contato@fmoliveira.com.br>
2013-09-04 21:22:13 -0300

    Fixing account controller.

--- Tigra/Fontes/Tigra/Tigra/App_Start/RouteConfig.cs
+++ Tigra/Fontes/Tigra/Tigra/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace Tigra
                 name: "Cells",
                 url: "{cell}/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", cell = "", action = "Index", id = UrlParameter.Optional },
-                constraints: new { cell = "^(?!.*(?i:Elicitation|Documentation|Revision|Baseline)).*$" }
+                constraints: new { cell = "^(?!.*(?i:Elicitation|Documentation|Revision|Baseline|Account)).*$" }
             );
 
             /* Default page routing. */
