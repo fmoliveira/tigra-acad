a35f0c67a7a65fa8ae8ff4ac4b4ae07683e6e9a7
Filipe Oliveira <contato@fmoliveira.com.br>
2013-11-04 01:56:40 -0200

    Removendo critérios de avaliação

--- Tigra/Database/Tables/RequirementRatings.sql
+++ Tigra/Database/Tables/RequirementRatings.sql
@@ -4,11 +4,6 @@ BEGIN
 	CREATE TABLE [Tigra].[RequirementRatings]
 	(
 		[RevisionID] BIGINT NOT NULL,
-		[RatingA] DECIMAL(3,1) NOT NULL CHECK([RatingA] BETWEEN 0 AND 5),
-		[RatingB] DECIMAL(3,1) NOT NULL CHECK([RatingB] BETWEEN 0 AND 5),
-		[RatingC] DECIMAL(3,1) NOT NULL CHECK([RatingC] BETWEEN 0 AND 5),
-		[FinalRating] AS (([RatingA] + [RatingB] + [RatingC]) / 3.0),
-		[Approved] BIT NOT NULL,
 		CONSTRAINT [pk_Tigra_RequirementRatings_Key] PRIMARY KEY ([RevisionID]),
 		CONSTRAINT [fk_Tigra_RequirementRatings_Rev] FOREIGN KEY ([RevisionID]) REFERENCES [Tigra].[RequirementRevisions] ([RevisionID])
 	);
--- Tigra/Database/Tables/UserRatings.sql
+++ Tigra/Database/Tables/UserRatings.sql
@@ -5,11 +5,8 @@ BEGIN
 	(
 		[RevisionID] BIGINT NOT NULL,
 		[UserID] INT NOT NULL,
-		[RatingA] TINYINT NOT NULL CHECK([RatingA] BETWEEN 0 AND 5),
-		[RatingB] TINYINT NOT NULL CHECK([RatingB] BETWEEN 0 AND 5),
-		[RatingC] TINYINT NOT NULL CHECK([RatingC] BETWEEN 0 AND 5),
-		[FinalRating] AS (([RatingA] + [RatingB] + [RatingC]) / 3.0),
 		[Comments] VARCHAR(250) NOT NULL,
+		[Approved] BIT NOT NULL,
 		CONSTRAINT [pk_Tigra_UserRatings_Key] PRIMARY KEY ([RevisionID], [UserID]),
 		CONSTRAINT [fk_Tigra_UserRatings_Rev] FOREIGN KEY ([RevisionID]) REFERENCES [Tigra].[RequirementRevisions] ([RevisionID]),
 		CONSTRAINT [fk_Tigra_UserRatings_Usr] FOREIGN KEY ([UserID]) REFERENCES [Tigra].[UserAccounts] ([UserId])
