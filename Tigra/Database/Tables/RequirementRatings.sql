611253ab9ba40ab849bc7fa8515bfe8a4fe9ff0d
Filipe Oliveira <contato@fmoliveira.com.br>
2013-11-17 21:36:09 -0200

    Banco de dados local.

--- Tigra/Database/Tables/RequirementRatings.sql
+++ Tigra/Database/Tables/RequirementRatings.sql
@@ -4,6 +4,7 @@ BEGIN
 	CREATE TABLE [Tigra].[RequirementRatings]
 	(
 		[RevisionID] BIGINT NOT NULL,
+		[Approved] BIT NOT NULL,
 		CONSTRAINT [pk_Tigra_RequirementRatings_Key] PRIMARY KEY ([RevisionID]),
 		CONSTRAINT [fk_Tigra_RequirementRatings_Rev] FOREIGN KEY ([RevisionID]) REFERENCES [Tigra].[RequirementRevisions] ([RevisionID])
 	);
